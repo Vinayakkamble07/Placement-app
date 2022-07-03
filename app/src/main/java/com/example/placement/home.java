876486d49c40ecd2c649fef0538c934161451660
Vinayak <vinayakkamble26@gmail.com>
2022-07-03 12:30:06 +0530

    placement app

--- app/src/main/java/com/example/placement/home.java
+++ app/src/main/java/com/example/placement/home.java
@@ -16,9 +16,7 @@ import java.util.ArrayList;
 
 public class home extends AppCompatActivity {
 
-    RecyclerView rcv;
-    Adapter adapter;
-
+    CardView c1,c2,c3,c4,c5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,43 +24,46 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        rcv=(RecyclerView)findViewById(R.id.rclview);
-        rcv.setLayoutManager(new LinearLayoutManager(this));
+        c1=(CardView) findViewById(R.id.c1);
+        c2=(CardView) findViewById(R.id.c2);
+        c3=(CardView) findViewById(R.id.c3);
+        c4=(CardView) findViewById(R.id.c4);
+        c5=(CardView) findViewById(R.id.c5);
 
-        adapter=new Adapter(dataqueue(),getApplicationContext());
-        rcv.setAdapter(adapter);
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this,CseActivity.class));
+            }
+        });
 
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this,EceActivity.class));
+            }
+        });
 
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this,EeeActivity.class));
+            }
+        });
 
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this,CivilActivity.class));
+            }
+        });
 
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(home.this,MechActivity.class));
+            }
+        });
 
-
-
-
-
-    }
-    public ArrayList<Model> dataqueue(){
-        ArrayList<Model> holder =new ArrayList<>();
-
-        Model ob1= new Model();
-        ob1.setImgname(R.drawable.logo1);
-        holder.add(ob1);
-
-        Model ob2= new Model();
-        ob2.setImgname(R.drawable.logo2);
-        holder.add(ob2);
-
-        Model ob3= new Model();
-        ob3.setImgname(R.drawable.logo3);
-        holder.add(ob3);
-
-        Model ob4= new Model();
-        ob4.setImgname(R.drawable.logo4);
-        holder.add(ob4);
-
-        Model ob5= new Model();
-        ob5.setImgname(R.drawable.logo5);
-        holder.add(ob5);
-        return holder;
     }
 }
